43f3a2e10c19ec29e1e29030fd80e0ce937abfe6
devid-koch <devidkoch33@gmail.com>
2024-02-07 08:31:27 +0530

    contents

--- src/pages/home/to-the-jouney.tsx
+++ src/pages/home/to-the-jouney.tsx
@@ -7,12 +7,12 @@ const ToTheJourney = () => {
       <div class="bg-white h-screen items-center flex">
         <div class="flex flex-col justify-center items-center space-y-8">
           <p class="uppercase text-lg">TO THE JOURNEY AHEAD</p>
-          <p class="uppercase text-gray-800 text-5xl text-center leading-tight">EXPERIENCE IN SCHOOL <br /> LEADERSHIP AND TEACHING</p>
+          <p class="uppercase text-gray-800 text-5xl text-center leading-tight">EXPERIENCE IN DEPARTMENT <br /> LEARNING AND DOING SOMETHING</p>
           <div class="w-1/2 text-center text-gray-800 text-lg fontText">We are creating an extraordinary and unique school.
-            Kingster College is the first
-            truly global and modern institution serving children from ages 3 to 18.
-            We not only want to improve our students’ lives,
-            but we also hope to contribute to the cause of education on every continent.</div>
+            The Computer Science Department at BCA is
+            dedicated to fostering a dynamic learning environment,
+            equipping students with the latest advancements in technology,
+            and nurturing a passion for innovation and problem-solving.</div>
           <button class="border-b border-gray-500 text-gray-500">
             Learn More
           </button>
